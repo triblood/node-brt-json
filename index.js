2350a907cae620d0e91034beb462bf37cc369f23
Davi Camarinha <davi.camarinha@fulllab.com.br>
2019-02-18 16:31:32 -0300

    Melhorias na api

--- index.js
+++ index.js
@@ -3,13 +3,36 @@ const restify = require('restify');
 const corsMiddleware = require('restify-cors-middleware');
 
 function listaLinha(req, res, next){
-    
-
     axios.get('http://webapibrt.rio.rj.gov.br/api/v1/brt')
     .then(function(response){
         let busLista = [];
+        console.log(req.params.linha)
+        for (let y = 0; y < req.params.linha.split(';').length; y++) {
+            
+            for (let x = 0; x < response.data.veiculos.length; x++) {
+                if((response.data.veiculos[x].linha == req.params.linha.split(';')[y]) && (response.data.veiculos[x].velocidade > 0)){
+                    // console.log(response.data.veiculos[x]);
+                    busLista.push(response.data.veiculos[x]);
+                }
+            }
+        }
+        
+        // console.log(busLista)
+        res.send(busLista);
+        // console.log(busLista)
+        next();
+    })
+    .catch(function(err){
+        console.log(err)
+    })
+}
+
+function onibusId(req, res, next){
+    axios.get('http://webapibrt.rio.rj.gov.br/api/v1/brt')
+    .then(function(response){
+        let busLista = []; 
         for (let x = 0; x < response.data.veiculos.length; x++) {
-            if((response.data.veiculos[x].linha == req.params.linha) && (response.data.veiculos[x].velocidade > 0)){
+            if((response.data.veiculos[x].codigo == req.params.busid) && (response.data.veiculos[x].velocidade > 0)){
                 // console.log(response.data.veiculos[x]);
                 busLista.push(response.data.veiculos[x]);
             }
@@ -23,8 +46,6 @@ function listaLinha(req, res, next){
     .catch(function(err){
         console.log(err)
     })
-
-    
 }
 
 // axios.get('http://webapibrt.rio.rj.gov.br/api/v1/brt')
@@ -49,6 +70,9 @@ server.use(cors.actual)
 
 server.get('/linha/:linha', listaLinha);
 server.head('/linha/:linha', listaLinha);
+
+server.get('/busid/:busid', onibusId);
+// server.head('/busid/:busid', listaLinha);
 
 server.listen(8080, function() {
     console.log('%s listening at %s', server.name, server.url);
